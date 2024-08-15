4213dde585524c6318e64e62071796e2fc0b865a
Diego <diego.valdez.r@tecsup.edu.pe>
2024-08-15 00:49:05 -0500

    V1 first version

---
+++ hello-world-nodejs/index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+const process = require('process');
+
+var expressapp = express()
+expressapp.get('/', function (req, res) {
+    res.send('{"message":"Hola Mundo desde JavaScript V1"}')
+})
+expressapp.listen(5000, function () {
+    console.log('Escuchando desde puerto 5000!')
+})
+
+/* Ctrl C*/
+process.on('SIGINT', function () {
+    process.exit();
+});
